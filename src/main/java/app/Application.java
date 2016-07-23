1a2f50684302894163956e64691cf9cc025222ae
Naga Srinivas <nagasai.srinu@gmail.com>
2016-07-23 18:39:46 +0530

    added db and view

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -11,7 +11,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.orm.jpa.vendor.HibernateJpaSessionFactoryBean;
 
 /**
  *
